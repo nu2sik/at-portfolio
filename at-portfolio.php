545be13f816fbd98183cc2491709da45a8196ed0
Anna <nu2sik@gmail.com>
2017-09-22 19:29:03 +0300

    Start create plugin

---
+++ at-portfolio.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: AT-Portfolio Plugin
+Plugin URI: https://codetiburon.com/
+Description: Portfolio plugin for courses WP.
+Version: 1.0
+Author: Anna Tarakanova
+Author URI: skype - nu2sik
+Text Domain: at-portfolio
+*/
+
+
+?>
